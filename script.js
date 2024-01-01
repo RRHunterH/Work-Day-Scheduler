f528b686b87b9145ba93e5e33ac22719b00d41f0
jimhunterangie Hurst  .̲͚͍̬̩͇͙̬̮̹͎͖̘̹͂̓̒̈́̾̈́̾͑́͋͋͋̓ͣ̀ <hangingwithhunter4@gmail.com>
2024-01-01 16:16:39 -0500

    finished the script.js, added 9 - 5 in index.html

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 $(function () {
     $(".saveBtn").on("click", function () {
-      var timeBlockId = $(this).parent().attr("id");
-      var userDescription = $(this).siblings(".description").val();
-      localStorage.setItem(timeBlockId, userDescription);
+        var timeBlockId = $(this).parent().attr("id");
+        var userDescription = $(this).siblings(".description").val();
+        localStorage.setItem(timeBlockId, userDescription);
     });
-  
+
     var currentHour = dayjs().hour();
     $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
-      if (blockHour < currentHour) {
-        $(this).addClass("past");
-      } else if (blockHour === currentHour) {
-        $(this).addClass("present");
-      } else {
-        $(this).addClass("future");
-      }
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        if (blockHour < currentHour) {
+            $(this).removeClass("present future").addClass("past");
+        } else if (blockHour === currentHour) {
+            $(this).removeClass("past future").addClass("present");
+        } else {
+            $(this).removeClass("past present").addClass("future");
+        }
     });
-  
+
     $(".time-block").each(function () {
-      var timeBlockId = $(this).attr("id");
-      var storedDescription = localStorage.getItem(timeBlockId);
-      $(this).find(".description").val(storedDescription);
+        var timeBlockId = $(this).attr("id");
+        var storedDescription = localStorage.getItem(timeBlockId);
+        $(this).find(".description").val(storedDescription);
     });
-  
+
     $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
-  });
-  
+});
